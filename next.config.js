055a68707a9cc911cc1d29ae8d50c89a5d8ecc75
Alessandro Alessio <info@a2area.it>
2024-04-22 08:04:51 +0200

    Testing for PWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 
+const prod = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
 	reactStrictMode: true, // Enable React strict mode for improved error handling
 	swcMinify: true, // Enable SWC minification for improved performance
@@ -10,9 +12,9 @@ const nextConfig = {
 
 const withPWA = require('next-pwa')({
 	dest: 'public',
-	// disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+	disable: prod ? false : true,
 	register: true, // Register the PWA service worker
-	skipWaiting: true, // Skip waiting for service worker activation
+	skipWaiting: false, // Skip waiting for service worker activation
 });
 
 // export default nextConfig;
